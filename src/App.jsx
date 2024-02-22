3f059c0f33de013c97cfc983c8e3f3ed458d08ac
gourav-pal <your.email@example.com>
2024-02-23 00:53:57 +0530

    implementing router dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import './App.css'
 import Hero from './components/Hero.jsx'
 import ProductCard from './components/ProductCard.jsx';
 import Footer from './components/Footer.jsx';
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Notfound from './pages/Notfound.jsx';
 
 function App() {
   return (
     <div>      
       < Header />
-      < Hero />
-      <div className="card1">
-      <ProductCard  />
-      </div>
+       <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/*' element={<Notfound/>}/>
+       </Routes>
       <Footer />
     </div>
      )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import ProductCard from '../components/ProductCard'
+
+const Home = () => {
+  return (
+    <div>
+        < Hero />
+      <div className="card1">
+      <ProductCard  />
+      </div>
+    </div>
+  )
+}
+
+export default Home
